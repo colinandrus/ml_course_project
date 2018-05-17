1d8236236b87c8b9810e23c31998f057edc48dc9
Gary Ng <nyjgary@gmail.com>
2018-05-17 01:42:19 -0400

    put model functions in a util python file

---
+++ processing_scripts/model_utils.py
@@ -0,0 +1,316 @@
+import numpy as np
+import pandas as pd 
+from sklearn.ensemble import RandomForestClassifier 
+from sklearn.model_selection import train_test_split 
+from sklearn.metrics import roc_curve, auc, roc_auc_score, accuracy_score, log_loss, \
+    precision_score, recall_score, precision_recall_curve, average_precision_score
+from sklearn.pipeline import make_pipeline, Pipeline 
+from sklearn.base import TransformerMixin, BaseEstimator
+from sklearn.feature_selection import SelectFromModel
+import matplotlib.pyplot as plt
+import os
+from xgboost import XGBClassifier
+
+# function to load data 
+
+# def load_data(filename, root_path=DATAFOLDER):
+#     csv_path = os.path.join(root_path, filename)
+#     df = pd.read_csv(csv_path)
+#     return df 
+
+
+# impute missing data
+
+def impute_columns_udf(col, imputation_type): 
+    """ 
+    UDF to specify imputation method for a given variable. 
+    col is the pd.Series for which you want to impute, imputation_type should be chosen from 
+    ['mode', 'mean', 'median', 'zero', 'none'] 
+    """ 
+    if imputation_type == 'mode':
+        val = col.mode()[0]
+        return col.fillna(val)
+    
+    elif imputation_type == 'mean': 
+        val = col.mean() 
+        return col.fillna(val)
+    
+    elif imputation_type == 'median': 
+        val = col.median() 
+        return col.fillna(val)
+    
+    elif imputation_type == 'zero': 
+        return col.fillna(0)
+    
+    elif imputation_type == 'none':
+        return col.fillna('None')
+    
+    else: 
+        raise ValueError('imputation_type argument not valid')
+
+class ImputeMissingData(BaseEstimator, TransformerMixin):
+    """ Pipeline step that impute missing values, given impute methods specified. 
+        Note that numerical features with NaN's and no impute methods will raise an error, 
+        categorical features on the other hand will be filled with 'None' by default unless specified otherwise.
+    """
+    def __init__(self, impute_methods, num_features, cat_features): 
+        self.impute_methods = impute_methods
+        self.num_features = num_features
+        self.cat_features = cat_features
+    
+    def fit(self, X, y=None): 
+        return self 
+    
+    def transform(self, input_data): 
+        output_data = input_data.copy() 
+
+        # check which features have missing values but imputation methods not specified 
+        features_nulls = output_data.isnull().sum() 
+        features_nulls = features_nulls[features_nulls > 0].index.tolist() 
+        features_need_impute = [f for f in features_nulls if f not in self.impute_methods.keys()]
+        num_features_need_impute = [f for f in features_need_impute if f in self.num_features]
+        cat_features_need_impute = [f for f in features_need_impute if f in self.cat_features]
+        
+        # raise exception for numerical features with missing values and no imputation method specified 
+        if num_features_need_impute: 
+            raise Exception("""These numerical features have missing values: {}. 
+            Please specify their impute methods.""".format(num_features_need_impute)) 
+        
+        # set imputation method as 'none' for cat features with missing values with no imputation method specified 
+        if cat_features_need_impute: 
+            for cat_f in cat_features_need_impute: 
+                self.impute_methods[cat_f] = 'none'
+            print("""{} have missing values with no imputation method specified. 
+            By default, they have been filled with 'None'.""".format(cat_features_need_impute)) 
+                
+        # apply imputations 
+        for col, imp_method in self.impute_methods.items(): 
+            output_data[col] = impute_columns_udf(output_data[col], imp_method)
+            
+        return output_data 
+
+
+# convert data types
+
+class ConvertDataTypes(BaseEstimator, TransformerMixin): 
+    """ Coerce data types to specifications defined by CAT_FEATURES and NUM_FEATURES """
+    def __init__(self, num_features, cat_features): 
+        self.num_features = num_features 
+        self.cat_features = cat_features
+    
+    def fit(self, X, y=None):
+        return self 
+    
+    def transform(self, input_data): 
+        output_data = input_data.copy() 
+        output_data[self.num_features] = output_data[self.num_features].astype(float)
+        output_data[self.cat_features] = output_data[self.cat_features].astype(str)
+        return output_data 
+
+
+# dummify data 
+
+class Dummify(BaseEstimator, TransformerMixin): 
+    """ Pipeline step that dummifies all categorical variables """
+    def __init__(self, cat_feature_values): 
+        self.cat_feature_values = cat_feature_values
+    
+    def fit(self, X, y=None): 
+        return self
+    
+    def transform(self, input_data):
+        output_data = input_data.copy()
+        # specify values categorical features can take on, to ensure train/test DF have same cols 
+        cat_features = self.cat_feature_values.keys()
+        for col in cat_features: 
+            output_data[col] = pd.Categorical(output_data[col], categories=self.cat_feature_values[col])        
+        output_data = pd.get_dummies(output_data, columns=cat_features, prefix_sep=':::')
+        return output_data
+
+
+def transform_features(data, df, cat_features, num_features, impute_methods): 
+    """ Impute, convert and dummify features 
+        TODO: save cat_feature_values to pickle so that we don't have to read df 
+    """  
+    
+    # subsets features we are interested in 
+    cat_feature_values = dict([(f, [str(x) for x in df[f].dropna().unique().tolist()]) for f in cat_features])
+    data = data[cat_features + num_features].copy() 
+
+    # make pipeline 
+    data_pipeline = Pipeline([
+        ('impute', ImputeMissingData(impute_methods, num_features, cat_features)),         
+        ('convert_dtypes', ConvertDataTypes(num_features, cat_features)), 
+        ('dummify', Dummify(cat_feature_values))
+    ])
+    
+    # run pipeline 
+    X = data_pipeline.fit_transform(data)
+    
+    return X
+
+# method to split train-test and transform features (for aggregate model)
+
+def get_model_data(df, label, cat_features, num_features, impute_methods, test_size=0.2, print_summary=False): 
+    """ Subsets features used, splits into train-test, and transforms features """
+    
+    # subsets features we are interested in 
+    data = df[cat_features + num_features + [label]].copy() 
+    
+    # train test split 
+    X, y = data.drop(label, axis=1).copy(), data[label].copy() 
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=44)
+
+    # transform features
+    X_train = transform_features(x_train, df, cat_features, num_features, impute_methods)
+    X_test = transform_features(x_test, df, cat_features, num_features, impute_methods)
+
+    if print_summary:
+        print("Training Data: {} | Test Data: {}".format(X_train.shape, X_test.shape)) 
+    
+    return X_train, X_test, y_train, y_test 
+
+# method to split train-test and transform features (for sequential models)
+
+def get_model_data_by_year(df, appeals_df, label, unique_id, cat_features, num_features, impute_methods, predict_year): 
+    """ Generates features on data for years prior to predict_year """
+
+    # add 'datAppealFiled_year' if not in num_features 
+    if 'datAppealFiled_year' not in num_features: 
+        num_features_ = num_features + ['datAppealFiled_year'] 
+    else: 
+        num_features_ = num_features 
+
+    # subsets features we are interested in 
+    cat_feature_values = dict([(f, [str(x) for x in df[f].dropna().unique().tolist()]) for f in cat_features]) 
+    data = df.set_index(unique_id)
+    if label is not None: 
+        data = data[cat_features + num_features_ + [label]].copy() 
+    else: 
+        data = data[cat_features + num_features].copy()
+
+    # train test split 
+    train_data = data[data['datAppealFiled_year'] < predict_year]
+    test_data = data[data['datAppealFiled_year'] == predict_year]
+    
+    # return y=None if label is not passed (used for parsing data for pure predictions)
+    if label is not None: 
+        x_train, y_train = train_data.drop(label, axis=1).copy(), train_data[label].copy() 
+        x_test, y_test = test_data.drop(label, axis=1).copy(), test_data[label].copy() 
+    else: 
+        x_train, y_train = train_data, None 
+        x_test, y_test = test_data, None 
+
+    # transform 
+    X_train = transform_features(x_train, appeals_df, cat_features, num_features, impute_methods)
+    X_test = transform_features(x_test, appeals_df, cat_features, num_features, impute_methods)
+
+    return X_train, X_test, y_train, y_test 
+
+# method to return model evaluation metrics 
+
+def evaluate_model(truth, pred): 
+    """ Takes in arrays of truth and pred y values and return accuracy, logloss, roc_auc, and plot ROC """ 
+    accuracy = accuracy_score(truth, (pred>0.5).astype(int))
+    logloss = log_loss(truth, pred)
+    fpr, tpr, thresholds = roc_curve(truth, pred)
+    roc_auc = auc(fpr, tpr)
+    precision = precision_score(truth, (pred>0.5).astype(int))
+    recall = recall_score(truth, (pred>0.5).astype(int))
+    metrics = {'Accuracy': accuracy, 'ROC AUC': roc_auc, 'Log Loss': logloss, 
+               'Precision': precision, 'Recall': recall}
+    return metrics
+
+
+# method to plot ROC 
+
+def plot_roc(truth, pred, model_name=None, title=None): 
+    """ Takes in arrays of truth classes and pred probs to plot ROC curve """
+    fpr, tpr, thresholds = roc_curve(truth, pred)
+    roc_auc = auc(fpr, tpr)
+    if model_name is not None: 
+        plt.plot(fpr, tpr, label= '{0} (AUC = {1:.3f})'.format(model_name, roc_auc)) 
+    else: 
+        plt.plot(fpr, tpr, label= 'AUC:{0:.3f}'.format(roc_auc)) 
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+    plt.xlabel('FPR')
+    plt.ylabel('TPR')
+    if title is not None: 
+        plt.title(title)
+    else: 
+        plt.title('ROC')
+    plt.legend(loc="lower right")   
+
+
+# method to plot precision-recall curve 
+
+def plot_precision_recall(truth, pred, model_name=None, title=None): 
+    """ Takes in arrays of truth classes and pred probs to plot precision-recall curve
+        Code borrowed from http://scikit-learn.org/stable/auto_examples/model_selection/plot_precision_recall.html
+    """    
+    precision, recall, _ = precision_recall_curve(truth, pred)
+    average_precision = average_precision_score(truth, pred)
+    plt.step(recall, precision, color='b', alpha=0.2, where='post')
+    plt.fill_between(recall, precision, step='post', alpha=0.2, color='b', 
+                     label='Avg Precision:{0:.3f}'.format(average_precision))
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    if title is not None: 
+        plt.title(title)
+    else: 
+        plt.title('Precision-Recall curve') 
+    plt.legend(loc='lower right')
+
+
+# method to train a model, output results, and plot AUC 
+
+def train_model(model, X_train, y_train, X_test, y_test, 
+                print_metrics=True, print_charts=False, sample_weight=None):
+    if sample_weight is not None: 
+        model.fit(X_train, y_train.values.ravel(), sample_weight)
+    else: 
+        model.fit(X_train, y_train.values.ravel())
+    truth = y_test.values.ravel()
+    pred = model.predict_proba(X_test)[:,1]
+    metrics = evaluate_model(truth, pred)
+    try: 
+        fi, cfi = get_feature_importances(model, X_train)
+    except AttributeError: 
+        fi, cfi = None, None 
+    
+    if print_metrics: 
+        print(metrics)
+    if print_charts: 
+        plot_metrics(truth, pred)
+    return model, metrics, fi, cfi, truth, pred   
+
+
+# method to plot ROC and Precision-Recall
+
+def plot_metrics(truth, pred): 
+    """ Plots ROC and Precision-Recall curves """
+    plt.figure(figsize=(12, 4))
+    plt.subplot(1, 2, 1)
+    plot_roc(truth, pred)
+    plt.subplot(1, 2, 2)
+    plot_precision_recall(truth, pred) 
+
+ # method to get feature importances 
+
+def get_feature_importances(model, X_train):
+    
+    """ Takes model and train data as inputs, outputs regular and collapsed feature importances """
+    
+    # get 'regular' feature importances 
+    fi = pd.Series(data=model.feature_importances_, index=X_train.columns).sort_values(ascending=False)
+    
+    # get 'collapsed' feature importances (dummy variables of parent feature aggregated as one)
+    cfi = pd.DataFrame(fi).reset_index().rename(columns={'index': 'feature', 0: 'importance'})
+    cfi['parent_feature'] = cfi['feature'].apply(lambda x: x.split(':::')[0])
+    cfi = cfi.groupby('parent_feature')['importance'].sum().sort_values(ascending=False) 
+    
+    return fi, cfi 
